331c461035ac541f6a02c713a0c5b1d1b14a0d75
Micah Neitz <micalobia2@gmail.com>
2020-08-22 01:50:14 -0700

    Cleaned up CustomLightSource

--- Assets/Assets/Scripts/CustomLightSource.cs
+++ Assets/Assets/Scripts/CustomLightSource.cs
@@ -6,31 +6,16 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class CustomLightSource : MonoBehaviour
 {
-    [SerializeField] public float FOV = 90f;
+    [SerializeField] [Range(0f, 360f)] public float FOV = 90f;
     [SerializeField] public float ViewDistance = 3f;
-    [SerializeField] public float Angle = 0f;
+    [SerializeField] public int rayCount = 360;
     [SerializeField] public LayerMask LayerMask;
 
-    private Vector3 origin = Vector3.zero;
-    private int rayCount = 360;
+    private Vector3 origin;
     private float startingAngle => transform.eulerAngles.z + FOV / 2f;
 
     private Mesh mesh;
-    public Mesh Mesh => mesh;
     private PolygonCollider2D polycol;
-
-    private static Vector3 GetVectorFromAngle(float angle)
-    {
-        float angleRad = angle * (Mathf.PI / 180f);
-        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
-    }
-    private static float GetAngleFromVectorFloat(Vector3 dir)
-    {
-        dir.Normalize();
-        float n = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
-        if (n < 0) n += 360;
-        return n;
-    }
 
     void Start()
     {
@@ -52,7 +37,8 @@ public class CustomLightSource : MonoBehaviour
         // Vector3[] verts = mesh.vertices;
         // List<Vector2> ret = new List<Vector2>()
         polycol.enabled = false;
-        polycol.SetPath(0, mesh.ToPolygon());
+        Vector2[] a;
+        polycol.SetPath(0, a = mesh.ToPolygon());
         polycol.enabled = true;
     }
 
@@ -70,7 +56,7 @@ public class CustomLightSource : MonoBehaviour
         float angleChange = FOV / rayCount;
         for (int i = 0; i <= rayCount; i++)
         {
-            Vector3 vecAng = GetVectorFromAngle(angle);
+            Vector3 vecAng = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
             Vector3 vertex;
             RaycastHit2D rayHit = Physics2D.Raycast(origin, vecAng, ViewDistance, LayerMask);
             vertex = (rayHit.collider == null) ? origin + vecAng * ViewDistance : (Vector3)rayHit.point; //Get where we hit if we hit, otherwise just put the point at our view distance
