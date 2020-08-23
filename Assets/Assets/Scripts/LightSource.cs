a1962be7012996acd22d30ddba5f6a26a166c982
Micah <micalobia2@gmail.com>
2020-08-22 19:05:14 -0700

    Added some stuff, it reflects but you can't make an overlapping poly

--- Assets/Assets/Scripts/LightSource.cs
+++ Assets/Assets/Scripts/LightSource.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(MeshCollider))]
 [RequireComponent(typeof(MeshFilter))]
 public class LightSource : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class LightSource : MonoBehaviour
 
     private Mesh mesh;
     private PolygonCollider2D polycol;
+    private MeshCollider meshcol;
 
     void Reset()
     {
@@ -33,7 +35,8 @@ public class LightSource : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        polycol = GetComponent<PolygonCollider2D>();
+        //polycol = GetComponent<PolygonCollider2D>();
+        meshcol = GetComponent<MeshCollider>();
         _reflective = LayerMask.GetMask("Reflective");
         _opaque = LayerMask.GetMask("Opaque");
     }
@@ -41,8 +44,8 @@ public class LightSource : MonoBehaviour
     void Update()
     {
         SetOrigin(transform.position);
-        GenerateMesh();
-        GeneratePolyCollider();
+       meshcol.sharedMesh = GenerateMesh();
+        //GeneratePolyCollider();
     }
 
     void GeneratePolyCollider()
@@ -57,63 +60,101 @@ public class LightSource : MonoBehaviour
 
     Mesh GenerateMesh()
     {
-        List<Vector3> vertices = new List<Vector3>(RayCount * 2);
+        List<Vector3?> vertices = new List<Vector3?>(RayCount * 4 + 5);
         List<int> triangles = new List<int>();
 
-        vertices[0] = Vector3.zero;
+        vertices.Add(origin);
         float angle = startingAngle;
         int vertexIndex = 1, triangleIndex = 0;
         float angleChange = FOV / RayCount;
 
         Vector3?[][] verts = new Vector3?[RayCount + 2][];
-        for (int i = 0; i <= RayCount; i++)
+        for (int i = 0; i <= RayCount; ++i)
         {
             Vector3 ang = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
             angle -= angleChange;
             Vector3?[] cast = CastRay(ang);
             verts[i] = cast;
         }
-        for (int i = 0; i < RayCount; i++)
+        //for (int i = 0; i < RayCount; i++)
+        //{
+        //    Vector3 vertex = verts[0][i+1] ?? Vector3.zero;
+        //    vertices.Add((vertex - transform.position).Rotate(-transform.eulerAngles.z));
+        //    if (i != 0)
+        //    {
+        //        triangles.Add(0);
+        //        triangles.Add(vertexIndex - 1);
+        //        triangles.Add(vertexIndex);
+        //        triangleIndex += 3;
+        //    }
+        //    vertexIndex++;
+        //}
+        for (int i = 0; i < 4; ++i)
+            for (int j = 0; j <= RayCount; ++j)
+                vertices.Add(verts[j][i]);
+        for (int i = 2; i <= RayCount; ++i)
         {
-            Vector3 vertex = verts[0][i+1] ?? Vector3.zero;
-            vertices.Add((vertex - transform.position).Rotate(-transform.eulerAngles.z));
-            if (i != 0)
-            {
-                triangles.Add(0);
-                triangles.Add(vertexIndex - 1);
-                triangles.Add(vertexIndex);
-                triangleIndex += 3;
-            }
-            vertexIndex++;
+            triangles.Add(0);
+            triangles.Add(i - 1);
+            triangles.Add(i);
         }
-        for (int i = 1; i < 4; i++)
-            for (int j = 0; j <= RayCount; j++)
-            {
-                Vector3? verty = verts[i][vertexIndex];
-            }
-
-
-        Vector2[] uv = new Vector2[vertices.Length];
-
-        for (int i = 0; i <= RayCount; i++)
+        int width = RayCount + 1;
+        for (int i = 1; i < 4; ++i)
         {
-            Vector3 vecAng = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
-            Vector3 vertex = CastRay(vecAng);
-            //vertex = (rayHit.collider == null) ? origin + vecAng * ViewDistance : (Vector3)rayHit.point; //Get where we hit if we hit, otherwise just put the point at our view distance
-            vertices[vertexIndex] = (vertex - transform.position).Rotate(-transform.eulerAngles.z);
-            if (i != 0)
+            for (int j = 0; j <= RayCount; ++j)
             {
-                triangles[triangleIndex] = 0;
-                triangles[triangleIndex + 1] = vertexIndex - 1;
-                triangles[triangleIndex + 2] = vertexIndex;
-                triangleIndex += 3;
+                int refl = i * width + 1;
+                if (!vertices[refl].HasValue) continue;
+                int prerefl = refl - 1;
+                if (!vertices[prerefl].HasValue) continue;
+                int main = (i - 1) * width + 1;
+                int premain = main - 1;
+                triangles.Add(premain);
+                triangles.Add(main);
+                triangles.Add(prerefl);
+                triangles.Add(main);
+                triangles.Add(refl);
+                triangles.Add(prerefl);
             }
-            vertexIndex++;
-            angle -= angleChange;
         }
-        mesh.vertices = vertices;
-        mesh.uv = uv;
-        mesh.triangles = triangles;
+        for (int i = 0; i < vertices.Count; ++i)
+        {
+            if (!vertices[i].HasValue)
+            {
+                vertices.RemoveAt(i--);
+                for (int j = 0; j < triangles.Count; ++j)
+                    if (triangles[j] > i) --triangles[j];
+            }
+            else
+            {
+                vertices[i] = (vertices[i].Value - transform.position).Rotate(-transform.eulerAngles.z);
+            }
+        }
+
+
+        //for (int i = 0; i <= RayCount; ++i)
+        //{
+        //    Vector3 vecAng = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+        //    Vector3 vertex = CastRay(vecAng);
+        //    //vertex = (rayHit.collider == null) ? origin + vecAng * ViewDistance : (Vector3)rayHit.point; //Get where we hit if we hit, otherwise just put the point at our view distance
+        //    vertices[vertexIndex] = (vertex - transform.position).Rotate(-transform.eulerAngles.z);
+        //    if (i != 0)
+        //    {
+        //        triangles[triangleIndex] = 0;
+        //        triangles[triangleIndex + 1] = vertexIndex - 1;
+        //        triangles[triangleIndex + 2] = vertexIndex;
+        //        triangleIndex += 3;
+        //    }
+        //    vertexIndex++;
+        //    angle -= angleChange;
+        //}
+
+        // Vector2[] uv = new Vector2[vertices.Count];
+
+        var filter = GetComponent<MeshFilter>();
+        mesh.triangles = triangles.ToArray();
+        mesh.vertices = (from v in vertices select new Vector3(v.Value.x,v.Value.y)).ToArray();
+        mesh.uv = new Vector2[mesh.vertices.Length];
         return mesh;
     }
 
@@ -127,10 +168,10 @@ public class LightSource : MonoBehaviour
         {
             int layer = 1 << rayHit.collider.gameObject.layer;
             ret[0] = rayHit.point;
-            if(depth == 1 || layer == opaque) return ret;
+            if (depth == 1 || layer == opaque) return ret;
             Vector3 norm = rayHit.normal;
-            Vector3 refAng = angle-2*Vector3.Dot(angle,norm)*norm;
-            Vector3?[] cast = CastRay(refAng, distance - rayHit.distance, ret[0], depth - 1);
+            Vector3 refAng = angle - 2 * Vector3.Dot(angle, norm) * norm;
+            Vector3?[] cast = CastRay(refAng, distance - rayHit.distance - 1, ret[0]+refAng, depth - 1);
             for (int i = 0; i < cast.Length; i++) ret[i + 1] = cast[i];
             return ret;
         }
