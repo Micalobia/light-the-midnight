0320b9d73587351b6e519d6f60795e0c823c24cf
Micah <micalobia2@gmail.com>
2020-08-27 17:16:45 -0700

    Fixed some Roak issues, still got some work on it
    Need to slap a light on the player, and give it the ability to attach to a trigger

--- Assets/Assets/Scripts/Roak.cs
+++ Assets/Assets/Scripts/Roak.cs
@@ -2,108 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AudioSource))]
 public class Roak : MonoBehaviour, IEnemy
 {
     #region Variables
-    public virtual float health => Health;
-    [SerializeField]
-    private float spriteOffset;
-    [SerializeField]
-    protected float Health = 100;
-    [SerializeField]
-    private Animator roakAnim;
-    [SerializeField]
-    private BoxCollider2D roakHitBox;
-    [SerializeField]
-    private float speed;
-    [SerializeField]
+    public float health => Health;
+    [SerializeField] private float SpriteOffset;
+    [SerializeField] protected float Health = 100;
+    public BoxCollider2D roakHitBox;
+    [SerializeField] public float Speed;
+    [SerializeField] public float AgroDistance;
+    [SerializeField] public float AttackDistance;
+    [SerializeField] public GameObject damageField;
+    [SerializeField] public float Timer;
+    [Header("Audio sources")]
+    [SerializeField] private AudioSource roakAudioSource;
+    [SerializeField] private AudioClip agroAudio;
+    [SerializeField] private AudioClip deathAudio;
+
     private Transform player;
-    [SerializeField]
-    private float agroDistance;
-    private Rigidbody2D roakRb;
-    [SerializeField]
-    private bool isFacingLeft;
-    [SerializeField]
-    private float attackDistance;
-    [SerializeField]
-    public GameObject damageField;
-    [SerializeField]
-    private bool hasSpawned;
-    [SerializeField]
-    private Vector3 startPosition;
-    [SerializeField]
-    private float Scale;
-    [SerializeField]
-    private float Timer;
-
-    //Sound Files for Roak Damage
-    [SerializeField]
-    private AudioSource roakAudioSource;
-
-    [SerializeField]
-    private AudioClip agroAudio;
-    [SerializeField]
-    private AudioClip deathAudio;
-
     private bool soundPlayed;
-
+    private Rigidbody2D roakRb;
+    private Animator roakAnim;
+    private bool IsFacingLeft;
+    private bool hasSpawned;
+    private float scaleX;
+    private float scaleY;
     #endregion
 
 
     void Awake()
     {
+        roakHitBox = GetComponent<BoxCollider2D>();
         roakAnim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         roakRb = GetComponent<Rigidbody2D>();
         roakAudioSource = GetComponent<AudioSource>();
         hasSpawned = false;
+        scaleX = transform.localScale.x;
+        scaleY = transform.localScale.y;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        CheckDistance();
+    void Update() => CheckDistance();
 
-       
-    }
 
-   
 
     #region Movement
     public void CheckDistance()
     {
         if (hasSpawned == true)
         {
-            
-
-            if (Vector2.Distance(transform.position, player.transform.position) < agroDistance)
+            if (Vector2.Distance(transform.position, player.transform.position) < AgroDistance)
             {
                 if (!soundPlayed)
                 {
                     roakAudioSource.PlayOneShot(agroAudio);
                     soundPlayed = true;
                 }
-
                 roakAnim.SetBool("isWalkingNearPlayer", true);
-
                 if (player.transform.position.y <= 10)
                 {
-
                     if ((player.transform.position.x + transform.position.x) > 0)
                     {
-                        transform.localScale = new Vector3(-.5f, .5f);
+                        transform.localScale = new Vector3(-scaleX, scaleY);
                         Attack();
                     }
                     if ((player.transform.position.x - transform.position.x) < 0)
                     {
-                        transform.localScale = new Vector2(.5f, .5f);
+                        transform.localScale = new Vector2(scaleX, scaleY);
                         Attack();
-
                     }
-
-                    transform.position = Vector2.MoveTowards(transform.position, new Vector2 (player.transform.position.x, transform.position.y), speed * Time.deltaTime);
-
+                    transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, transform.position.y), Speed * Time.deltaTime);
                 }
 
             }
@@ -113,27 +85,27 @@ public class Roak : MonoBehaviour, IEnemy
                 roakAnim.SetBool("isWalking", true);
 
 
-                if (isFacingLeft && Timer < 5)
+                if (IsFacingLeft && Timer < 5)
                 {
-                    transform.Translate(-speed * Time.deltaTime, 0f, 0f);
-                    transform.localScale = new Vector2(Scale, Scale);
+                    transform.Translate(-Speed * Time.deltaTime, 0f, 0f);
+                    transform.localScale = new Vector2(scaleX, scaleY);
                     Timer += Time.deltaTime;
 
-                    if(Timer > 5)
+                    if (Timer > 5)
                     {
-                        isFacingLeft = false;
+                        IsFacingLeft = false;
                         Timer = 0;
                     }
                 }
-                else if (!isFacingLeft && Timer < 5)
+                else if (!IsFacingLeft && Timer < 5)
                 {
-                    transform.Translate(speed * Time.deltaTime, 0f, 0f);
-                    transform.localScale = new Vector2(-Scale, Scale);
+                    transform.Translate(Speed * Time.deltaTime, 0f, 0f);
+                    transform.localScale = new Vector2(-scaleX, scaleY);
                     Timer += Time.deltaTime;
 
-                    if(Timer > 5)
+                    if (Timer > 5)
                     {
-                        isFacingLeft = true;
+                        IsFacingLeft = true;
                         Timer = 0;
                     }
                 }
@@ -147,7 +119,7 @@ public class Roak : MonoBehaviour, IEnemy
     {
         Health -= damage;
 
-        if(health <= 0)
+        if (health <= 0)
         {
             roakAudioSource.PlayOneShot(deathAudio);
 
@@ -163,59 +135,56 @@ public class Roak : MonoBehaviour, IEnemy
             }
             else
             {
-                transform.position = new Vector2(transform.position.x, transform.position.y - spriteOffset);
+                transform.position = new Vector2(transform.position.x, transform.position.y - SpriteOffset);
             }
             roakHitBox.enabled = false;
-            
+
         }
     }
 
     public void OnCollisionEnter2D(Collision2D objEnv)
     {
-        if (objEnv.gameObject.CompareTag("Obstacle") && isFacingLeft == true)
+        if (objEnv.gameObject.CompareTag("Obstacle") && IsFacingLeft == true)
         {
-           
-          isFacingLeft = false;
+
+            IsFacingLeft = false;
             Timer = 0;
-          
+
         }
-        else if (objEnv.gameObject.CompareTag("Obstacle") && isFacingLeft == false)
+        else if (objEnv.gameObject.CompareTag("Obstacle") && IsFacingLeft == false)
         {
-            isFacingLeft = true;
+            IsFacingLeft = true;
             Timer = 0;
         }
-       
+
     }
 
-    public void Death()
-    {
-        Destroy(this.gameObject);
-    }
+    public void Death() => Destroy(gameObject);
 
     public void Attack()
     {
-        if(Vector2.Distance(transform.position, player.transform.position) < attackDistance)
+        if (Vector2.Distance(transform.position, player.transform.position) < AttackDistance)
         {
             roakAnim.SetTrigger("Attack");
         }
     }
 
-    public void spawn()
+    public void Spawn()
     {
         roakAnim.SetTrigger("hasSpawned");
         hasSpawned = true;
-     
     }
 
-    public void enableHitbox()
+    public void EnableHitBox()
     {
-        if(damageField.activeInHierarchy == false)
-        {
-            damageField.SetActive(true);
-        }
-        else if(damageField.activeInHierarchy == true)
-        {
-            damageField.SetActive(false);
-        }
+        damageField.SetActive(!damageField.activeInHierarchy);
+        //if (damageField.activeInHierarchy == false)
+        //{
+        //    damageField.SetActive(true);
+        //}
+        //else if (damageField.activeInHierarchy == true)
+        //{
+        //    damageField.SetActive(false);
+        //}
     }
 }
