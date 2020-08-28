6bcba9c32caf6523dde94cf5f1ab7763fda9eace
Matt <mtryszkowski@gmail.com>
2020-08-27 17:25:50 -0700

    Character Name is relevant

---
+++ Assets/Assets/Scripts/RecallUsername.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecallUsername : MonoBehaviour
+{
+    public Text textDisplay;
+
+    void Start()
+    {
+        string name = UsernameManager.PlayerName;
+        textDisplay.text = name;
+    }
+}
