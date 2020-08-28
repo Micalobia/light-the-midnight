26f2c0e0573609a4743845c9bbb2e589c3d07e4c
JakeRe <jake.reilly.20@gmail.com>
2020-08-28 08:55:46 -0500

    Began Work on Boss Enemy

---
+++ Assets/Assets/opening_cutscene/Scripts/BossController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator bossAnim;
+    private int tentacleNum;
+
+    void Awake()
+    {
+        bossAnim = GetComponent<Animator>();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tentacleNum = Random.Range(0, 5);
+        bossAnim.SetInteger("ActiveTentacle", tentacleNum);
+        tentacleNum = 0;
+    }
+
+    IEnumerator TentacleSelect()
+    {
+       
+
+        yield return new WaitForSecondsRealtime(3);
+       
+    }
+}
