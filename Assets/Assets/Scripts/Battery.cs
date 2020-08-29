1f1b9296f7ce3886a260d3f8b6d8982f78178f27
Micah <micalobia2@gmail.com>
2020-08-29 02:04:23 -0700

    Added Battery

---
+++ Assets/Assets/Scripts/Battery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+class Battery : MonoBehaviour
+{
+    private Animator anim;
+    private Weapon player;
+    private int charges;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Weapon>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(player.ChargesLeft);
+        if (player.ChargesLeft != charges)
+        {
+            charges = player.ChargesLeft;
+            anim.SetInteger("ChargesLeft", charges);
+        }
+    }
+}
+
