ec6dd0d43ebc2ecadba198e73d14d62d77246296
JakeRe <jake.reilly.20@gmail.com>
2020-08-29 21:54:55 -0500

    StartedFleshPuddle.FuckYou

---
+++ Assets/DetectEnemies.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectEnemies : MonoBehaviour
+{
+    [SerializeField] List<Collider2D> enemies = new List<Collider2D>();
+    [SerializeField] private float detectRadius;
+    [SerializeField] private LayerMask enemiesLM;
+
+    void Update()
+    {
+       
+    }
+    private void FixedUpdate()
+    {
+        if(enemies.Count == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
