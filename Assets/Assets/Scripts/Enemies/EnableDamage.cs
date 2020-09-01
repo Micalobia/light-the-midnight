b05f6c7936f270c658a1a6a6cb62bca4a76ab951
Micah <micalobia2@gmail.com>
2020-09-01 16:17:11 -0700

    Made the Boss able to take damage

--- Assets/Assets/Scripts/Enemies/EnableDamage.cs
+++ Assets/Assets/Scripts/Enemies/EnableDamage.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnableDamage : MonoBehaviour, IEnemy
 {
 
-    public float health => Health; 
+    public float health => Health;
     [SerializeField] private float Health = 1;
     [SerializeField] private Animator bossAnim;
     [SerializeField] private BossController bossController;
@@ -13,20 +14,30 @@ public class EnableDamage : MonoBehaviour, IEnemy
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private GameObject poof;
     [SerializeField] private Animator poofAnim;
+    [SerializeField] private int index;
 
     void Awake()
     {
         bossAnim = GetComponentInParent<Animator>();
         bossController = GetComponentInParent<BossController>();
         poof.SetActive(false);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Weapon>().OnLightTrigger += OnLightTrigger;
     }
-    
-   
+
+    private void OnLightTrigger(ref Collider2D collider)
+    {
+        if (health > 0 && collider.gameObject == gameObject && bossAnim.GetBool("EyeIsActive") && bossAnim.GetInteger("ActiveEye") == index)
+        {
+            //The if-statement here is supposed to check if the eye is open before damaging, but it just... doesn't?
+            takeDamage(1f);
+        }
+    }
+
     public virtual void takeDamage(float damage)
     {
         Health -= damage;
 
-        if(health <= 0)
+        if (health <= 0)
         {
             StartCoroutine("EyeDeath");
         }
@@ -39,7 +50,7 @@ public class EnableDamage : MonoBehaviour, IEnemy
         poof.SetActive(true);
         poofAnim.SetTrigger("poofActive");
         yield return new WaitForSeconds(poofAnim.GetCurrentAnimatorStateInfo(0).length + poofAnim.GetCurrentAnimatorStateInfo(0).normalizedTime);
-        Destroy(this.gameObject);
+        Destroy(gameObject);
         audioSource.PlayOneShot(hurtclip);
     }
 
