594224b1b521e2070f24f17922a77f47ae2b0270
Micah <micalobia2@gmail.com>
2020-09-03 00:17:09 -0700

    ToScene prefab added

---
+++ Assets/Assets/Scripts/Interface/SendToScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(InteractSender))]
+class SendToScene : MonoBehaviour
+{
+    [SerializeField] private string scene;//
+    private void Awake() => GetComponent<InteractSender>().OnInteract += Send;
+    private void Send() => SceneManager.LoadScene(scene);
+}
