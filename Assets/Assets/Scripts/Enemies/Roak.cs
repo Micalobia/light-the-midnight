57cadf53090a4a2ae7690f6871440af15c374571
Micah <micalobia2@gmail.com>
2020-09-02 23:07:06 -0700

    Temporary zoop

--- Assets/Assets/Scripts/Enemies/Roak.cs
+++ Assets/Assets/Scripts/Enemies/Roak.cs
@@ -91,7 +91,7 @@ class Roak : MonoBehaviour
             _anim.SetBool("Ready", _ready = true);
         };
         LightSourceHolder[] holders = FindObjectsOfType<LightSourceHolder>();
-        foreach(var holder in holders)
+        foreach (var holder in holders)
         {
             if (holder.transform.parent != null && holder.transform.parent.TryGetComponent(out LightSourceHolder _)) continue;
             holder.OnLightTrigger += OnLightTrigger;
@@ -143,13 +143,13 @@ class Roak : MonoBehaviour
     private void SetAgro(bool value)
     {
         if (value && !_agro)
-        _roakaudio.PlayOneShot(OnAgroClip);
+            _roakaudio.PlayOneShot(OnAgroClip);
         _agro = value;
         _anim.SetBool("Agro", value);
     }
     private void Agro()
     {
-        if(!_player.activeInHierarchy)
+        if (!_player.activeInHierarchy)
         {
             Patrol();
             return;
@@ -168,16 +168,13 @@ class Roak : MonoBehaviour
     private void StartDeath()
     {
         if (!_dead)
-        _dead = true;
+            _dead = true;
         _anim.SetBool("Dead", true);
         _mainCol.enabled = false;
     }
 
 
-    private void DeathNoise()
-    {
-        _roakaudio.PlayOneShot(OnDeathClip);
-    }
+    private void DeathNoise() => _roakaudio.PlayOneShot(OnDeathClip);
     private void Die() => Destroy(gameObject);
     private void Spawned() => _spawned = true;
 }
