4535e2e656743a964b069aefc74fa0ecde75dab6
Matt <mtryszkowski@gmail.com>
2020-09-03 01:53:03 -0500

    Cake Boss Entrance

---
+++ Assets/Assets/Scripts/Interface/Level Building/EntranceTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class EntranceTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueTrigger;
+    [SerializeField] private GameObject[] Enemies;
+
+    private void Update()
+    {
+        for (int i = 0; i < Enemies.Length; i++) if (!Enemies[i].IsDestroyed()) return;
+        dialogueTrigger.SetActive(true);
+    }
+}
